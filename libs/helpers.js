548d731c21607b441cf270b60986d67d9584b0da
anton <anton@univ.net.ua>
2013-03-24 15:06:48 +0200

    csv parser: reduce memory and cpu consumption
    make csv parser to consume less memory and cpu time
    by writing code, in way, to reduce internal v8's
    memory management overhead

--- libs/helpers.js
+++ libs/helpers.js
@@ -58,76 +58,72 @@ Helpers.prototype = {
     Assert.deepEqual(parseCsv('11,22,33,"'), ['11', '22', '33', ''], "unclosed before end");
     Assert.deepEqual(parseCsv('",sdfasd'), [',sdfasd'], "unclosed before coma");
     Assert.deepEqual(parseCsv('"aa","'), ['aa', ''], 'unclosed before end after escaped');
-    Assert.deepEqual(parseCsv('","aa","asdfas"'), [',"aa', 'asdfas'], 'this is error csv, so everything is acceptable');*/
+    Assert.deepEqual(parseCsv('","aa","asdfas"'), [',"aa', 'asdfas'], 'this is error csv, so everything is acceptable');
+    */
     var i,
       currentChar,
-      currentField = '',
-      fieldIsEscaped = false,
-      inField = false,
+      NOT_FIELD = 0,
+      FIELD = 1,
+      FIELD_ESCAPED = 2,
+      FIELD_ESCAPED_QUOTE = 3,
+      state = {state: NOT_FIELD, start: 0, length: 0},
       row = [];
-    if (string.length !== 0) {
-      for (i = 0; i <= string.length; i++) {
-        if (i === string.length) {
-          row.push(currentField);
-          continue;
-        }
-        currentChar = string[i];
-        if (currentChar === '"') {
-          if (inField) {
-            if (fieldIsEscaped) {
-              if (i < string.length - 1) {
-                if (string[i + 1] === '"') {
-                  i++;
-                } else if (string[i + 1] === ',') {
-                  row.push(currentField);
-                  currentField = '';
-                  inField = false;
-                  continue;
-                }
-              } else {
-                inField = false;
-              }
-            }
-          } else {
-            inField = true;
-            fieldIsEscaped = true;
-            continue;
-          }
-        } else if (currentChar === ',') {
-          if (inField) {
-            if (!fieldIsEscaped) {
-              row.push(currentField);
-              currentField = '';
-              inField = false;
-            }
-          }
-          if (!inField) {
-            if (i === 0) {
-              row.push('');
-            }
-            if (i === string.length - 1) {
-              row.push('');
-              break;
-            }
-            if (i < string.length - 1 && string[i + 1] === ',') {
-              row.push('');
-            }
-          }
+    if (string.length === 0) {
+      return [];
+    }
+    for (i = 0; i <= string.length; i++) {
+      currentChar = i < string.length ? string[i] : undefined;
+      if (state.state === NOT_FIELD) {
+        if (i === string.length || currentChar === ',') {
+          row.push('');
+          state.state = NOT_FIELD;
+          state.start = i;
+          state.end = 0;
+        } else if (currentChar === '"') {
+          state.state = FIELD_ESCAPED;
+          state.start = i + 1;
+          state.length = 0;
         } else {
-          if (!inField) {
-            inField = true;
-            fieldIsEscaped = false;
-          }
+          state.state = FIELD;
+          state.start = i;
+          state.length = 1;
         }
-        if (inField) {
-          currentField += currentChar;
+      } else if (state.state === FIELD) {
+        if (i === string.length || currentChar === ",") {
+          row.push(string.slice(state.start, state.start + state.length));
+          state.state = NOT_FIELD;
+          state.start = i;
+          state.length = 0;
+        } else {
+          state.length++;
+        }
+      } else if (state.state === FIELD_ESCAPED) {
+        if (i === string.length) {
+          row.push(string.slice(state.start, state.start + state.length).replace('""', '"'));
+          state.state = NOT_FIELD;
+          state.start = i;
+          state.length = 0;
+        } else if (currentChar === '"') {
+          state.state = FIELD_ESCAPED_QUOTE;
+        } else {
+          state.length++;
+        }
+      } else if (state.state === FIELD_ESCAPED_QUOTE) {
+        if (i === string.length || currentChar === ',') {
+          row.push(string.slice(state.start, state.start + state.length).replace('""', '"'));
+          state.state = NOT_FIELD;
+          state.start = i;
+          state.length = 0;
+        } else {
+          state.state = FIELD_ESCAPED;
+          state.length += 2;
         }
       }
     }
     return row;
   },
   findExecutable: (function () {
-    var execIndex = {}; 
+    var execIndex = {};
     return function (exec, cache, callback) {
       if (cache instanceof Function) {
         callback = cache;
@@ -139,7 +135,7 @@ Helpers.prototype = {
       ChildProcess.exec('which \'' + exec.replace('\'', '\\\'') + '\'', function (error, stdout, stderr) {
         if (error || stderr.length) {
           return callback(error || stderr.toString());
-        } 
+        }
         if (!stdout.length) {
           return callback(new Error(exec + "is not found. install it"));
         }
@@ -147,11 +143,11 @@ Helpers.prototype = {
           execIndex[exec] = stdout.toString().trim();
         }
         callback(null, stdout.toString().trim());
-      });  
-    }; 
+      });
+    };
   })()
 
 };
-module.exports.instance = function (Lib) {
+module.exports.instance = function () {
   return Helpers;
 };
